f0fa5263f531297f5ddfbbc91c28ec60913c283c
Tomeu Barcelo <tomeu.95@gmail.com>
2021-03-27 12:33:41 +0100

    creacio clase casella i fitxa

---
+++ src/main/java/com/prog/juegodefichas/Fitxa.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prog.juegodefichas;
+
+/**
+ *
+ * @author tomeu
+ */
+public class Fitxa {
+    //atributs
+    String color;
+    Casella c;
+    int fila, columna;
+}
