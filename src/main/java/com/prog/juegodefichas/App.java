1dd887175c1c60938974f6519f3e094caa122229
Tomeu Barcelo <tomeu.95@gmail.com>
2021-03-28 16:55:02 +0200

    *

--- src/main/java/com/prog/juegodefichas/App.java
+++ src/main/java/com/prog/juegodefichas/App.java
@@ -5,30 +5,32 @@
  */
 package com.prog.juegodefichas;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tomeu
  */
 public class App {
-    
-    public static void main(String args[]) {
-        
+           
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner (System.in);
         int [][]tablero = new int[5][5];
-        
-        for(int i=0;i<tablero.length;i++){
-            for(int j=0;j<tablero[i].length;j++){
-              //dp something with x[i][j]
-              tablero[i][j]= i+1;
-            }
-        }
+
         for (int i=0; i < tablero.length; i++) {
             for (int j=0; j < tablero[i].length; j++) {
+                tablero[i][j]= i+1;
                 System.out.print(tablero[i][j]+"\t");
             }
             System.out.println("");
         }
-        String colorFitxa = "blanc";
+        
+        System.out.println("¿Qué color quieres? (blanco o negro)");
+        String colorFitxa = sc.next();
+        comprovaColorFitxa(colorFitxa);
+        
         Casella posicioInicialFitxa = new Casella (1,1);
+        
         int vides = 1;
         Fitxa torre = new Torre (colorFitxa, posicioInicialFitxa, vides);
         System.out.println(torre.getColor());
@@ -36,4 +38,16 @@ public class App {
         System.out.println(posicioInicialFitxa.getFila());
     }
     
+    public static String comprovaColorFitxa(String color) throws Exception{
+        try {
+           if (color.equals("blanco") || color.equals("negro")) {
+               return color;
+            } else{
+                return null;
+            } 
+        } catch (Exception e) {
+             throw new Exception(e+" - color incorrecto");        
+        }
+
+    }
 }
