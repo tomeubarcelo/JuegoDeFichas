c2af6b9098711f00f5c0b15cec72af907e0b4abc
Tomeu Barcelo <tomeu.95@gmail.com>
2021-03-28 16:33:51 +0200

    wip array bidim. tablero

--- src/main/java/com/prog/juegodefichas/App.java
+++ src/main/java/com/prog/juegodefichas/App.java
@@ -13,6 +13,20 @@ public class App {
     
     public static void main(String args[]) {
         
+        int [][]tablero = new int[5][5];
+        
+        for(int i=0;i<tablero.length;i++){
+            for(int j=0;j<tablero[i].length;j++){
+              //dp something with x[i][j]
+              tablero[i][j]= i+1;
+            }
+        }
+        for (int i=0; i < tablero.length; i++) {
+            for (int j=0; j < tablero[i].length; j++) {
+                System.out.print(tablero[i][j]+"\t");
+            }
+            System.out.println("");
+        }
         String colorFitxa = "blanc";
         Casella posicioInicialFitxa = new Casella (1,1);
         int vides = 1;
